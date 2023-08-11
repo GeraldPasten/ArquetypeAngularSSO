ecb6eff71f6a5aff0c9c39817ff96389967d91ba
Gerald Pasten <gpasten@redhat.com>
2023-08-11 11:12:44 -0400

    SSO + variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,10 +12,10 @@
 
 export const environment = {
   production: false,
-  url: '${OPENSHIFT_URL}',
-  urlKeycloak: '${OPENSHIFT_SSO_URL}',
-  realm: '${OPENSHIFT_SSO_REALM}',
-  client: '${OPENSHIFT_SSO_CLIENT}'
+  url: process.env.OPENSHIFT_URL,
+  urlKeycloak: process.env.OPENSHIFT_SSO_URL,
+  realm: process.env.OPENSHIFT_SSO_REALM,
+  client: process.env.OPENSHIFT_SSO_CLIENT
 };
 
 /*
